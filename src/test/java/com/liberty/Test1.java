969dbd91f99b820e685536d5504961c98815a899
Swanand Mehta <Swanand.Mehta@kpit.com>
2020-11-23 20:16:33 +0530

    Commit-5

--- src/test/java/com/liberty/Test1.java
+++ src/test/java/com/liberty/Test1.java
@@ -5,6 +5,7 @@ package com.liberty;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,21 +15,25 @@ import org.junit.jupiter.api.Test;
 public class Test1 {
 	
 	@Test
+	@DisplayName("T1-T1")
 	public void test1() {
 		assertTrue(Core.isSuccess(), "Failed at test4");
 	}
 	
 	@Test
+	@DisplayName("T1-T2")
 	public void test2() {
 		assertTrue(Core.isSuccess(), "Failed at test4");
 	}
 	
 	@Test
+	@DisplayName("T1-T3")
 	public void test3() {
 		assertTrue(Core.isSuccess(), "Failed at test4");
 	}
 		
 	@Test
+	@DisplayName("T1-T4")
 	public void test4() {
 		assertTrue(Core.isSuccess(), "Failed at test4");
 	}
